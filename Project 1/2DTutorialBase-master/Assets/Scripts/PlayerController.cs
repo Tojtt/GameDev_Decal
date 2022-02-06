93b6276b563a60e9822cb997963f7ae1989e4ada
Hoang To <hoangto.losangeles@gmail.com>
2022-02-06 09:01:22 -0800

    PlayerController codes
    create regions + basic functions

--- Project 1/2DTutorialBase-master/Assets/Scripts/PlayerController.cs
+++ Project 1/2DTutorialBase-master/Assets/Scripts/PlayerController.cs
@@ -12,11 +12,65 @@ public class PlayerController : MonoBehaviour
     #endregion
 
     #region Physics_components
+    Rigidbody2D PlayerRB;
+    #endregion
+
+    #region Attack_variables
+    public float Damage; 
+    float attackspeed = 1;
+    float attackTimer;
+    public float hitboxtiming;
+    public float endanimationtiming;
+    bool isAttacking;
+    Vector2 currDirection;
     #endregion
 
     #region Unity_functions
+
+    private void Awake() 
+    {
+        PlayerRB = GetComponent<Rigidbody2D>();     
+    }
+
+    private void Update() 
+    {
+        x_input = Input.GetAxisRaw("Horizontal");
+        y_input = Input.GetAxisRaw("Vertical");
+
+        Move();
+    }
     #endregion
 
     #region Movment_functions
+
+    private void Move()
+    {
+        if(x_input > 0)
+        {
+            PlayerRB.velocity = Vector2.right * movespeed;
+        } 
+        else if (x_input < 0)
+        {
+            PlayerRB.velocity = Vector2.left * movespeed;
+        }
+
+        else if (y_input > 0)
+        {
+            PlayerRB.velocity = Vector2.up * movespeed;
+        } 
+        else if (y_input < 0)
+        {
+            PlayerRB.velocity = Vector2.down * movespeed;
+        }
+        else
+        {
+            PlayerRB.velocity = Vector2.zero;
+        }
+
+    }
+    #endregion
+
+    #region Attack_functions
+
     #endregion
 }
