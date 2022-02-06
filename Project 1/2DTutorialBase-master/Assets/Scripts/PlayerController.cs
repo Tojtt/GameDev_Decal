9dd4097e0f55bd10fb293325567ea66c0c7e9d66
Hoang To <hoangto.losangeles@gmail.com>
2022-02-06 00:52:28 -0800

    PlayerController
    starting up script file

---
+++ Project 1/2DTutorialBase-master/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    #region Movment_variables
+    public float movespeed;
+    float x_input;
+    float y_input; 
+    #endregion
+
+    #region Physics_components
+    #endregion
+
+    #region Unity_functions
+    #endregion
+
+    #region Movment_functions
+    #endregion
+}
