cdc27db05425181d5f59f9b7df88868ddf5d148d
Hoang To <hoang.to94@gmail.com>
2022-02-08 00:10:32 -0800

    Create Chest and Health Pack

---
+++ Project 1/2DTutorialBase-master/Assets/Scripts/Chest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    #region GameObject_variables
+    [SerializeField]
+    [Tooltip("health pack")]
+    private GameObject healthpack;
+    #endregion
+
+    #region Chest_functions
+
+    IEnumerator DestroyChest()
+    {
+        yield return new WaitForSeconds(.3f);
+        Instantiate(healthpack, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+    
+    public void Interact()
+    {
+        StartCoroutine("DestroyChest");
+    }
+
+    #endregion
+}
